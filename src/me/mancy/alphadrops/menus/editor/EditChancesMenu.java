331d7d79d0444ee08a8e5226d58e1c1b3d335a89
Mandrew1 <ajcarras@me.com>
2018-08-14 16:16:33 -0400

    Work on menus

---
+++ src/me/mancy/alphadrops/menus/editor/EditChancesMenu.java
@@ -0,0 +1,22 @@
+package me.mancy.alphadrops.menus.editor;
+
+import me.mancy.alphadrops.utils.Menu;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+
+public class EditChancesMenu extends Menu {
+    @Override
+    protected Inventory getInventory() {
+        return null;
+    }
+
+    @Override
+    protected void setUp() {
+
+    }
+
+    @Override
+    protected void handleInput(InventoryClickEvent event) {
+
+    }
+}
--- src/me/mancy/alphadrops/parties/DropParty.java
+++ src/me/mancy/alphadrops/parties/DropParty.java
@@ -2,6 +2,10 @@ package me.mancy.alphadrops.parties;
 
 import me.mancy.alphadrops.main.Main;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DropParty implements Listener {
 
@@ -12,13 +16,19 @@ public class DropParty implements Listener {
      */
 
     private int tier;
+    private List<ItemStack> itemList = new ArrayList<>();
 
     public DropParty(int tier) {
         this.tier = tier;
+        loadItemList(this.tier);
     }
 
     public DropParty(Main main) {
         main.getServer().getPluginManager().registerEvents(this, main);
+    }
+
+    private void loadItemList(int tier) {
+
     }
 
     public void start() {
